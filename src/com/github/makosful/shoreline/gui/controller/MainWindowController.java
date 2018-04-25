82f7560cfb3f32be03fcbf5555db74fdef89468f
dankphetamine <asge0907@easv365.dk>
2018-04-25 14:32:47 +0200

    code setup

--- src/com/github/makosful/shoreline/gui/controller/MainWindowController.java
+++ src/com/github/makosful/shoreline/gui/controller/MainWindowController.java
@@ -1,34 +1,109 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package com.github.makosful.shoreline.gui.controller;
 
-import com.github.makosful.shoreline.gui.model.MainWindowModel;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import org.controlsfx.control.CheckListView;
 
 /**
- * FXML Controller class.
- * This class connects the data from the MainWindowModel to the view, and
- * handles the input
+ * FXML Controller class
  *
- * @see MainWindowModel The Class handling the data for this MVC
- *
- * @author Axl
+ * @author Storm
  */
-public class MainWindowController implements Initializable
-{
+public class MainWindowController implements Initializable {
 
-    private MainWindowModel model;
+    //<editor-fold defaultstate="collapsed" desc="Split Pane Descriptions">
+    @FXML
+    private Color x211;
+    @FXML
+    private Font x111;
+    @FXML
+    private Color x2;
+    @FXML
+    private Font x1;
+    @FXML
+    private Color x21;
+    @FXML
+    private Font x11;
+//</editor-fold>
+
+    @FXML
+    private CheckListView<String> chklistSelectData;
+    @FXML
+    private ListView<String> listSorted;
+    @FXML
+    private Button btnMoveUp;
+    @FXML
+    private Button btnMoveDown;
+    @FXML
+    private Button btnConvert;
+    @FXML
+    private ColumnConstraints gridOutputColumn;
+
+    //<editor-fold defaultstate="collapsed" desc="Labels">
+    @FXML
+    private Label lblSiteName;
+    @FXML
+    private Label lblAssetSerialNo;
+    @FXML
+    private Label lblExtWorkOrderID;
+    @FXML
+    private Label lblSystemStatus;
+    @FXML
+    private Label lblUserStatus;
+    @FXML
+    private Label lblCreatedOn;
+    @FXML
+    private Label lblCreatedBy;
+    @FXML
+    private Label lblPriority;
+    @FXML
+    private Label lblStatus;
+    @FXML
+    private Label lblEarliestStart;
+    @FXML
+    private Label lblLatestStart;
+    @FXML
+    private Label lblLatestFinish;
+    @FXML
+    private Label lblEstimatedTime;
+    @FXML
+    private Label lblNameDescription;
+    @FXML
+    private Label lblOrderType;
+    //</editor-fold>
 
     /**
      * Initializes the controller class.
-     *
-     * @param url FXML Specific. Unused.
-     * @param rb  FXML Specific. Unused
      */
     @Override
-    public void initialize(URL url, ResourceBundle rb)
-    {
-        model = new MainWindowModel();
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+    @FXML
+    private void handleMoveItemUp(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleMoveItemDown(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleConversion(ActionEvent event) {
     }
 
 }
