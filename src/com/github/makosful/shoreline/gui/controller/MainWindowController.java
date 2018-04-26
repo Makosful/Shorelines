35e03d207f6a3341a0909116d5c4ac29fdd3ad25
dankphetamine <asge0907@easv365.dk>
2018-04-26 13:32:27 +0200

    -

--- src/com/github/makosful/shoreline/gui/controller/MainWindowController.java
+++ src/com/github/makosful/shoreline/gui/controller/MainWindowController.java
@@ -6,6 +6,7 @@
 package com.github.makosful.shoreline.gui.controller;
 
 import java.net.URL;
+import java.util.Collections;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener;
@@ -44,19 +45,6 @@ public class MainWindowController implements Initializable {
     private Font x11;
 //</editor-fold>
 
-    @FXML
-    private CheckListView<String> chklistSelectData;
-    @FXML
-    private ListView<String> listSorted;
-    @FXML
-    private Button btnMoveUp;
-    @FXML
-    private Button btnMoveDown;
-    @FXML
-    private Button btnConvert;
-    @FXML
-    private ColumnConstraints gridOutputColumn;
-
     //<editor-fold defaultstate="collapsed" desc="Labels">
     @FXML
     private Label lblSiteName;
@@ -90,27 +78,63 @@ public class MainWindowController implements Initializable {
     private Label lblOrderType;
     //</editor-fold>
 
-    final ObservableList<String> strings = FXCollections.observableArrayList("One 1", "Two 2", "Three 3", "Four 4", "Five 5", "Six 6", "Seven 7", "Eight 8", "Nine 9", "Ten10");
+    @FXML
+    private CheckListView<String> chklistSelectData;
+    @FXML
+    private ListView<String> listViewSorted;
+    @FXML
+    private Button btnMoveUp;
+    @FXML
+    private Button btnMoveDown;
+    @FXML
+    private Button btnConvert;
+    @FXML
+    private ColumnConstraints gridOutputColumn;
+
+    private Boolean movable;
+    private Integer currentIndex;
+
+    ObservableList<String> strings = FXCollections.observableArrayList("One 1", "Two 2", "Three 3", "Four 4", "Five 5", "Six 6", "Seven 7", "Eight 8", "Nine 9", "Ten10");
+    ObservableList<String> selectedStrings = FXCollections.observableArrayList();
 
     /**
      * Initializes the controller class.
+     *
+     * @param url
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
 
         chklistSelectData.setItems(strings);
 
+        listViewSorted.setItems(selectedStrings);
+
         AddListeners();
     }
 
     @FXML
     private void handleMoveItemUp(ActionEvent event) {
+        System.out.println("Moving up?");
+        if (movable) {
+            // Gets the two indexes
+            currentIndex = listViewSorted.getSelectionModel().getSelectedIndex();
+            int prevIndex = currentIndex - 1;
 
+            Collections.swap(selectedStrings, currentIndex, prevIndex);
+        } else {
+            // If the item can't be moved, return without doign anything
+            return;
+        }
     }
 
     @FXML
     private void handleMoveItemDown(ActionEvent event) {
-
+        currentIndex = listViewSorted.getSelectionModel().getSelectedIndex();
+        if (movable) {
+            //TODO - add "set new selected item/index"
+        } else {
+            return;
+        }
     }
 
     @FXML
@@ -118,15 +142,40 @@ public class MainWindowController implements Initializable {
 
     }
 
-    private void checkIfExisting() {
-        if (!listSorted.getItems().contains(chklistSelectData.getSelectionModel().getSelectedItem())) {
-            listSorted.setItems(chklistSelectData.getCheckModel().getCheckedItems());
+    private void checkIfValidToRelocate() {
+        if (listViewSorted.getSelectionModel().getSelectedIndex() > 0
+                && listViewSorted.getSelectionModel().getSelectedIndex() < selectedStrings.size()) {
+            movable = true;
+        } else {
+            movable = false;
+        }
+    }
+
+    private void disableBtnOnIndex() {
+        if (listViewSorted.getSelectionModel().getSelectedIndex() == 0) {
+            btnMoveUp.setDisable(true);
+        } else {
+            btnMoveUp.setDisable(false);
+        }
+        if (listViewSorted.getSelectionModel().getSelectedIndex() == listViewSorted.getItems().size() - 1) {
+            btnMoveDown.setDisable(true);
+        } else {
+            btnMoveDown.setDisable(false);
         }
     }
 
     private void AddListeners() {
         chklistSelectData.getCheckModel().getCheckedItems().addListener((ListChangeListener.Change<? extends String> c) -> {
-            checkIfExisting();
+            if (c.next()) {
+                selectedStrings.addAll(c.getAddedSubList());
+                selectedStrings.removeAll(c.getRemoved());
+            }
+        });
+
+        listViewSorted.getSelectionModel().selectedIndexProperty().addListener((observable) -> {
+            checkIfValidToRelocate();
+
+            disableBtnOnIndex();
         });
     }
 }
