7324505b60a3a20d0b33507dfe601462e0b69ae9
Axl <makosful@gmail.com>
2018-04-27 10:44:41 +0200

    Modualized the file reading for the Excel files

--- src/com/github/makosful/shoreline/dal/ExcelReader.java
+++ src/com/github/makosful/shoreline/dal/ExcelReader.java
@@ -3,13 +3,12 @@ package com.github.makosful.shoreline.dal;
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
-import org.apache.poi.hssf.usermodel.HSSFCell;
-import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.poifs.filesystem.POIFSFileSystem;
-import org.apache.poi.xssf.usermodel.XSSFCell;
-import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
@@ -36,47 +35,10 @@ public class ExcelReader
         HSSFWorkbook wb = new HSSFWorkbook(fs);
         HSSFSheet sheet = wb.getSheetAt(0);
 
-        // Location within the sheet
-        HSSFRow row;
-        HSSFCell cell;
-
-        int rows; // Number of rows
-        rows = sheet.getPhysicalNumberOfRows();
-
-        int cols = 0; // Number of columns
-        int tmp;
-
-        for (int i = 0; i < 10; i++)
-        {
-            row = sheet.getRow(i);
-            if (row != null)
-            {
-                tmp = sheet.getRow(i).getPhysicalNumberOfCells();
-                if (tmp > cols)
-                {
-                    cols = tmp;
-                }
-            }
-        }
-
-        for (int r = 0; r < rows; r++)
-        {
-            row = sheet.getRow(r);
-            if (row != null)
-            {
-                for (int c = 0; c < cols; c++)
-                {
-                    cell = row.getCell((short) c);
-                    if (cell != null)
-                    {
-                        // Code
-                    }
-                }
-            }
-        }
+        m(sheet);
     }
 
-    void readFromXlsxFiles(String file) throws IOException
+    public void readFromXlsxFiles(String file) throws IOException
     {
         // Set up
         XSSFWorkbook wb = new XSSFWorkbook(
@@ -84,16 +46,20 @@ public class ExcelReader
                         new FileInputStream(file)));
         XSSFSheet sheet = wb.getSheetAt(0);
 
-        // Location within the sheet
-        XSSFRow row;
-        XSSFCell cell;
+        m(sheet);
+    }
 
+    private void m(Sheet sheet)
+    {
+        Row row;
         int rows; // Number of rows
         rows = sheet.getPhysicalNumberOfRows();
 
+        Cell cell;
         int cols = 0; // Number of columns
         int tmp;
 
+        // Scans the first few rows in case the data starts lower
         for (int i = 0; i < 10; i++)
         {
             row = sheet.getRow(i);
@@ -107,6 +73,7 @@ public class ExcelReader
             }
         }
 
+        // The core loop that goes through every cell
         for (int r = 0; r < rows; r++)
         {
             row = sheet.getRow(r);
@@ -117,7 +84,7 @@ public class ExcelReader
                     cell = row.getCell((short) c);
                     if (cell != null)
                     {
-                        System.out.println(cell.getRawValue());
+                        System.out.println(cell.getAddress());
                     }
                 }
             }
