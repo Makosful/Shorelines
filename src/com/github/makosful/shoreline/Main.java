699fd8808d6c3e3311c6019a7d4266ebc443a151
Axl <makosful@gmail.com>
2018-04-30 15:42:32 +0200

    M

--- src/com/github/makosful/shoreline/Main.java
+++ src/com/github/makosful/shoreline/Main.java
@@ -1,9 +1,14 @@
 package com.github.makosful.shoreline;
 
+import com.github.makosful.shoreline.gui.model.Cache;
+import com.github.makosful.stage.exception.IlligalIdException;
+import com.github.makosful.stage.utils.StageManager;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
@@ -17,12 +22,31 @@ public class Main extends Application
     @Override
     public void start(Stage stage) throws Exception
     {
-        Parent root = FXMLLoader.load(getClass().getResource("gui/view/MainWindow.fxml"));
+        StageManager sm = new StageManager(stage);
+        Cache cache = Cache.getInstance();
+        cache.setStageManager(sm);
 
-        Scene scene = new Scene(root);
+        registerScenes(sm);
 
-        stage.setScene(scene);
-        stage.show();
+        sm.getStage().show();
+
+        File file = new File("./res/logoT.png");
+        Image icon = new Image(file.toURI().toString());
+        sm.getStage().getIcons().add(icon);
+    }
+
+    private void registerScenes(StageManager sm)
+    {
+        try
+        {
+            sm.registerScene(0, "Shoreline",
+                             getClass().getResource("gui/view/MainWindow.fxml"));
+            sm.setActiveScene(0);
+        }
+        catch (IlligalIdException | IOException ex)
+        {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
--- src/com/github/makosful/shoreline/gui/model/Cache.java
+++ src/com/github/makosful/shoreline/gui/model/Cache.java
@@ -1,5 +1,7 @@
 package com.github.makosful.shoreline.gui.model;
 
+import com.github.makosful.stage.utils.StageManager;
+
 /**
  * This Model class will serve as a data cache, storing data that should be
  * shared between Model classes.
@@ -26,8 +28,60 @@ public class Cache
     }
     //</editor-fold>
 
+    // Fields
+    private StageManager stageManager;
+
     // Singleton constructor
     private Cache()
     {
     }
+
+    //<editor-fold defaultstate="collapsed" desc="Stage Manager">
+    /**
+     * Gets the current Stage Manager
+     *
+     * @return Returns a reference to the current StageManager
+     */
+    public StageManager getStageManager()
+    {
+        return this.stageManager;
+    }
+
+    /**
+     * Sets the Stage Manager. This should typically only be done when the app
+     * starts
+     *
+     * @param sm The StageManage to use
+     */
+    public void setStageManager(StageManager sm)
+    {
+        this.stageManager = sm;
+    }
+
+    /**
+     * Clears the StageManager
+     */
+    public void clearStageManager()
+    {
+        this.stageManager = null;
+    }
+
+    /**
+     * Changes the Scene to the one with the same registered ID.
+     * The Scene IDs can be found in the Main class
+     *
+     * @see
+     * com.github.makosful.shoreline.Main#registerScenes(com.github.makosful.stage.utils.StageManager)
+     *
+     * @param sceneId The ID of the stage
+     */
+    public void changeScene(int sceneId)
+    {
+        stageManager.setActiveScene(sceneId);
+    }
+
+    public void m()
+    {
+    }
+    //</editor-fold>
 }
