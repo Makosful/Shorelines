04287cb17a6cc1c2d1bbb69a1dc910a8752a878d
davidmsf <davidmsf@gmail.com>
2018-05-08 11:30:47 +0200

    test

---
+++ test/com/github/makosful/shoreline/dal/ReaderFactoryTest.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.makosful.shoreline.dal;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author B
+ */
+public class ReaderFactoryTest
+{
+    
+    public ReaderFactoryTest()
+    {
+    }
+    
+    @BeforeClass
+    public static void setUpClass()
+    {
+    }
+    
+    @AfterClass
+    public static void tearDownClass()
+    {
+        
+    }
+
+    /**
+     * Test of getExtension method, of class ReaderFactory.
+     */
+    @Test
+    public void testGetExtension()
+    {
+    }
+    
+}
