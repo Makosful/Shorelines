1539311e6857ad1c96eb8a9a56691887477d4842
Husso1997 <hussain1961@hotmail.com>
2018-05-08 10:18:31 +0200

    .

--- src/com/github/makosful/shoreline/gui/controller/MainWindowController.java
+++ src/com/github/makosful/shoreline/gui/controller/MainWindowController.java
@@ -1,12 +1,9 @@
 package com.github.makosful.shoreline.gui.controller;
 
-import com.github.makosful.shoreline.Main;
 import com.github.makosful.shoreline.be.ColumnObject;
 import com.github.makosful.shoreline.be.Config;
 import com.github.makosful.shoreline.bll.BLLException;
 import com.github.makosful.shoreline.gui.model.MainWindowModel;
-import java.io.File;
-import java.io.IOException;
 import java.net.URL;
 import java.util.Collections;
 import java.util.List;
@@ -16,12 +13,9 @@ import javafx.collections.ObservableList;
 import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.*;
+<<<<<<< HEAD
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.paint.Color;
@@ -29,6 +23,11 @@ import javafx.scene.text.Font;
 import javafx.stage.FileChooser;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+=======
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+>>>>>>> parent of 95b4ae2... Added Instructions Window
 import javafx.util.StringConverter;
 import org.controlsfx.control.CheckListView;
 
@@ -114,8 +113,6 @@ public class MainWindowController implements Initializable
     private ComboBox<Config> comboBoxConfig;
     @FXML
     private TextField txtFieldConfig;
-    @FXML
-    private MenuItem menuItemInstructions;
 
     /**
      * Initializes the controller class.
@@ -258,6 +255,10 @@ public class MainWindowController implements Initializable
             }
             model.getSelectedList().removeAll(c.getRemoved());
         });
+<<<<<<< HEAD
+=======
+        
+>>>>>>> parent of 95b4ae2... Added Instructions Window
     }
 
     @FXML
@@ -335,16 +336,19 @@ public class MainWindowController implements Initializable
         {
         comboBoxConfig.valueProperty().addListener((obs, oldConfig, newConfig) ->
         {
+<<<<<<< HEAD
 
             Config config = model.getConfig(newConfig.getId());
             //listViewSorted.setItems(config.getChosenColumns());
             if (newConfig != null)
             {
+=======
+            if(newConfig != null){
+>>>>>>> parent of 95b4ae2... Added Instructions Window
                 chklistSelectData.getCheckModel().clearChecks();
-                if (!newConfig.getName().equals("No config"))
+                if(!newConfig.getName().equals("No config"))
                 {
-                    for (ColumnObject c : newConfig.getChosenColumns())
-                    {
+                    for(ColumnObject c : newConfig.getChosenColumns()){
                         chklistSelectData.getCheckModel().check(c.getColumnID());
                     }
                 }
@@ -367,7 +371,7 @@ public class MainWindowController implements Initializable
         model.saveConfig(txtFieldConfig.getText(), listViewSorted.getItems());
         comboBoxConfig.getItems().clear();
         addConfigs();
-        comboBoxConfig.getSelectionModel().select(comboBoxConfig.getItems().size() - 1);
+        comboBoxConfig.getSelectionModel().select(comboBoxConfig.getItems().size()-1);
     }
 
     @FXML
@@ -380,31 +384,5 @@ public class MainWindowController implements Initializable
     private void handleOpenLog(ActionEvent event)
     {
         model.openLogWindow();
-    }
-
-    @FXML
-    private void handleShowInstructionsWindow(ActionEvent event)
-    {
-        try
-        {
-            File file = new File("./res/logo.png");
-            Image icon = new Image(file.toURI().toString());
-
-            Stage stage = new Stage();
-            Parent root = FXMLLoader.load(Main.class.getResource("gui/view/HelpWindow.fxml"));
-            stage.setScene(new Scene(root));
-            stage.setTitle("Shoreline | Instructions");
-            stage.getIcons().add(icon);
-            stage.initModality(Modality.WINDOW_MODAL);
-            stage.initOwner(((Node) event.getSource()).getScene().getWindow());
-            stage.show();
-        }
-        catch (IOException ex)
-        {
-            System.out.println("failed to open the window");
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setContentText(ex.getMessage());
-            alert.show();
-        }
     }
 }
