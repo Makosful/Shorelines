736321a1459795ac0e66d37c2df90b1c2a283971
Axl <makosful@gmail.com>
2018-05-15 14:27:33 +0200

    Fixed it now?

--- src/com/github/makosful/shoreline/gui/model/LoginModel.java
+++ src/com/github/makosful/shoreline/gui/model/LoginModel.java
@@ -1,13 +1,10 @@
 package com.github.makosful.shoreline.gui.model;
 
-import com.github.makosful.shoreline.be.User;
 import com.github.makosful.shoreline.bll.BLLException;
 import com.github.makosful.shoreline.bll.BLLManager;
 import com.github.makosful.shoreline.bll.IBLL;
-import com.github.makosful.shoreline.gui.model.Cache.Scenes;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
 /**
@@ -20,14 +17,12 @@ public class LoginModel
     private final Cache cache;
     private final IBLL bll;
 
-    private final StringProperty message;
+    private StringProperty message;
 
     public LoginModel()
     {
         cache = Cache.getInstance();
         bll = new BLLManager();
-
-        message = new SimpleStringProperty();
     }
 
     public void savePassword(String userName, String password) throws BLLException
@@ -50,39 +45,6 @@ public class LoginModel
         {
             Logger.getLogger(LoginModel.class.getName()).log(Level.SEVERE, null, ex);
             return new String(); // Empty string
-        }
-    }
-
-    public StringProperty getMessageProperty()
-    {
-        return message;
-    }
-
-    public void openSignup()
-    {
-        cache.changeScene(Scenes.SignUp.getValue());
-    }
-
-    public void login(String uName, String pass)
-    {
-        try
-        {
-            final User u = bll.login(uName, pass);
-
-            if (u == null)
-            {
-                // Error Message
-                message.setValue("Wrong username or password");
-            }
-            else
-            {
-                cache.setUser(u);
-                cache.changeScene(Scenes.Main.getValue());
-            }
-        }
-        catch (BLLException ex)
-        {
-            Logger.getLogger(LoginModel.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 }
