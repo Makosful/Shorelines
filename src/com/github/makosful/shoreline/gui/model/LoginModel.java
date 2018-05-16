cca66cfad31bc6bfb7e8d70ac50c1ba620872222
Axl <makosful@gmail.com>
2018-05-16 10:09:04 +0200

    Finally fixed this git error

--- src/com/github/makosful/shoreline/gui/model/LoginModel.java
+++ src/com/github/makosful/shoreline/gui/model/LoginModel.java
@@ -1,12 +1,13 @@
 package com.github.makosful.shoreline.gui.model;
 
-import com.github.makosful.shoreline.be.User;
 import com.github.makosful.shoreline.bll.BLLException;
 import com.github.makosful.shoreline.bll.BLLManager;
 import com.github.makosful.shoreline.bll.IBLL;
 import com.github.makosful.shoreline.gui.model.Cache.Scenes;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 
 /**
  *
@@ -18,46 +19,26 @@ public class LoginModel
     private final Cache cache;
     private final IBLL bll;
 
+    private final StringProperty message;
+
     public LoginModel()
     {
         cache = Cache.getInstance();
         bll = new BLLManager();
+
+        message = new SimpleStringProperty();
     }
 
-    public boolean attemptLogin(String userName, String password)
-    {
-        boolean valid = true;
-        if (valid) // Replace with a proper test
-        {
-            // Create a new user based on the login
-            User user = new User();
-
-            // Give that user to Cache
-            cache.setUser(user);
-
-            // Change the screen to the main window
-            cache.changeScene(Scenes.Main.getValue());
-
-            // Return true for successful login
-            return true;
-        }
-        else
-        {
-            // Return false for failed login
-            return false;
-        }
-    }
-    
     public void savePassword(String userName, String password) throws BLLException
     {
         bll.savePassword(userName, password);
     }
-    
+
     public String[] getPassword() throws BLLException
     {
         return bll.getPassword();
     }
-    
+
     public String getNewPassword()
     {
         try
@@ -69,5 +50,27 @@ public class LoginModel
             Logger.getLogger(LoginModel.class.getName()).log(Level.SEVERE, null, ex);
             return new String(); // Empty string
         }
+    }
+
+    public StringProperty getMessageProperty()
+    {
+        return message;
+    }
+
+    public void login(String name, String pass)
+    {
+        try
+        {
+            bll.login(name, pass);
+        }
+        catch (BLLException ex)
+        {
+            // Handle properly later
+        }
+    }
+
+    public void openSignup()
+    {
+        cache.changeScene(Scenes.SignUp.getValue());
     }
 }
