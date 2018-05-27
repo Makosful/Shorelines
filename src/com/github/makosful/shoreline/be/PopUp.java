09e0c92f5e51b9dfaf08630f190dd9e4a9b2f5f1
dankphetamine <asge0907@easv365.dk>
2018-05-27 17:41:58 +0200

    Added Insert Custom

--- src/com/github/makosful/shoreline/be/PopUp.java
+++ src/com/github/makosful/shoreline/be/PopUp.java
@@ -5,11 +5,60 @@
  */
 package com.github.makosful.shoreline.be;
 
+import javafx.event.ActionEvent;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
 /**
  *
  * @author Storm
  */
-class PopUp
+public class PopUp
 {
 
+    private static String txtFromInput;
+
+    public static void display()
+    {
+        Stage popupwindow = new Stage();
+        popupwindow.initModality(Modality.APPLICATION_MODAL);
+        Label lbl1 = new Label("Enter your custom text below or leave empty");
+        TextField txtInput = new TextField();
+
+        popupwindow.setOnCloseRequest((event) ->
+        {
+            txtFromInput = "UNDEFINED";
+        });
+
+        txtInput.setOnAction((ActionEvent e) ->
+        {
+            if (txtInput.getText().isEmpty() || txtInput.getText() == null)
+            {
+                txtFromInput = "UNDEFINED";
+            }
+            else
+            {
+                txtFromInput = txtInput.getText();
+            }
+            popupwindow.close();
+        });
+
+        VBox layout = new VBox(20);
+        layout.getChildren().addAll(lbl1, txtInput);
+        layout.setAlignment(Pos.CENTER);
+        Scene scene1 = new Scene(layout, 250, 100);
+
+        popupwindow.setScene(scene1);
+        popupwindow.showAndWait();
+    }
+
+    public static String getInputText()
+    {
+        return txtFromInput;
+    }
 }
